adc1e51bd761badd255913d9798069c18dd1af85
yufyaj <yk1994k@gmail.com>
2024-07-23 12:09:55 +0900

    CORS対応

--- infra/main.tf
+++ infra/main.tf
@@ -290,6 +290,14 @@ resource "aws_api_gateway_method" "register_post" {
   authorization = "NONE"
 }
 
+# /register リソースの POST メソッドの作成
+resource "aws_api_gateway_method" "register_options" {
+  rest_api_id   = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id   = aws_api_gateway_resource.register.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
 # /login リソースの POST メソッドの作成
 resource "aws_api_gateway_method" "login_post" {
   rest_api_id   = aws_api_gateway_rest_api.tamotsu_api.id
@@ -314,6 +322,70 @@ resource "aws_api_gateway_integration" "register_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.register_function.invoke_arn
+}
+
+# /register メソッドのreponse
+resource "aws_api_gateway_integration_response" "post_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id = aws_api_gateway_resource.register.id
+  http_method = aws_api_gateway_method.register_post.http_method
+  status_code = aws_api_gateway_method_response.register_post_reponse.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+# /register メソッドのreponse
+resource "aws_api_gateway_method_response" "register_post_reponse" {
+  rest_api_id = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id = aws_api_gateway_resource.register.id
+  http_method = aws_api_gateway_method.register_post.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+# /register メソッドと Lambda 関数の連携
+resource "aws_api_gateway_integration" "register_options_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id             = aws_api_gateway_resource.register.id
+  http_method             = aws_api_gateway_method.register_options.http_method
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = jsonencode({
+      statusCode = 200
+    })
+  }
+}
+
+# OPTIONSメソッドのレスポンス
+resource "aws_api_gateway_method_response" "register_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id = aws_api_gateway_resource.register.id
+  http_method = aws_api_gateway_method.register_options.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+# OPTIONSメソッドの統合レスポンス
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.tamotsu_api.id
+  resource_id = aws_api_gateway_resource.register.id
+  http_method = aws_api_gateway_method.register_options.http_method
+  status_code = aws_api_gateway_method_response.register_options_response.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
 
 # /login メソッドと Lambda 関数の連携
